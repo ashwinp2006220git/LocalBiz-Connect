d4430ef5de8cee2a5f13e9fd9631ead92b72d4ce
ashwinp2006220git <ashwinp2006220@example.com>
2026-06-05 15:40:37 +0530

    app.py changes

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="frontend",
+    static_folder="frontend",
+    static_url_path=""
+)
 
 @app.route("/")
 def home():
